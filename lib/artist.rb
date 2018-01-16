185b9dceabfdcad6b9d02d3d0b938af2dbb98be2
Sharmina Akhtar <sakht318@gmail.com>
2018-01-16 11:12:25 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,29 +1,5 @@
 class Artist
+   attr_accessor :name, :songs, :artist
 
-  attr_accessor :name, :songs, :artist
-
-  @@song_count = 0
-
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
-
-  def add_song(song)
-    self.songs << song
-    song.artist = self
-    @@song_count += 1
-  end
-
-  def add_song_by_name(name)
-    song = Song.new(name)
-    self.songs << song
-    song.artist = self
-    @@song_count += 1
-  end
-
-  def self.song_count
-    @@song_count
-  end
 
 end
