5622d7df04ad424d2c158e9a5d27cd32c8873936
Sharmina Akhtar <sakht318@gmail.com>
2018-01-16 11:29:57 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,4 +1,26 @@
 class Author
    attr_accessor :name, :posts
+   
+      def initialize(name)
+        @name = name
+        @posts = []
+      end
 
+      def add_song(post)
+        @posts << self
+        post.artist = self
+      end
+
+      def add_song_by_name(title)
+        post = Author.new(title, author)
+        @posts << post
+        post.author = self
+      end
+
+      def posts
+        @posts
+     end
+
+
+   
 end
