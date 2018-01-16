95ecd968c23ca011071016a2dadb5e4af42dd8f4
Sharmina Akhtar <sakht318@gmail.com>
2018-01-16 11:25:37 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,24 @@
 class Artist
    attr_accessor :name, :songs, :artist
 
+   def initialize(name)
+     @name = name
+     @songs = []
+   end
+
+   def add_song(song)
+     @songs << self
+     song.artist = self
+   end
+
+   def add_song_by_name(name)
+     song = Song.new(name, artist)
+     @songs << song
+     song.artist = self
+
+   def songs
+     @songs
+  end
+
 
 end
