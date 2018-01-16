5709158fedf181a582a74165671b07c2f4d672c4
Sharmina Akhtar <sakht318@gmail.com>
2018-01-16 11:27:01 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,16 +2,9 @@ class Song
 
   attr_accessor :name, :artist
 
-  @@all = []
-
   def initialize(name)
     @name = name
     @artist = artist
-    @@all << self
-  end
-
-  def self.all
-    @@all
   end
 
   def artist_name
