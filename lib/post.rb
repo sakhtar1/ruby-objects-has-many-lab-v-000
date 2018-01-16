715ee3c8caba6a5f9106aa5190674ccf654acb0a
Sharmina Akhtar <sakht318@gmail.com>
2018-01-16 11:21:38 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,4 +1,24 @@
 class Post
   attr_accessor :title, :author
 
+    @@post = []
+
+    def initialize(title, author = nil)
+      @title = title
+      @author = author
+      @@post << self
+    end
+
+    def self.all
+      @@post
+    end
+
+    def artist_name
+      if self.author
+        self.author.title
+      else
+        nil
+      end
+    end
+
 end
